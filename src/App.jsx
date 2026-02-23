5d16f0f15717f16736b02264c51a10be23f428bc
Nelson Romaine <nelson@romaine.life>
2026-02-22 17:37:36 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{
+      margin: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      background: '#000',
+      fontFamily: 'sans-serif',
+    }}>
+      <h1 style={{ color: '#fff', fontSize: '4rem', textAlign: 'center' }}>
+        ahh my bhole
+      </h1>
+    </div>
+  )
+}
+
+export default App
